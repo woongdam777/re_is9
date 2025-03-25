d1328d69ab25d62326ca8b4b88adb24e11007824
home <woongdam777@gmail.com>
2025-03-25 13:03:44 +0900

    fix

--- src/app/components/Section/SectionHome.js
+++ src/app/components/Section/SectionHome.js
@@ -12,16 +12,17 @@ export default function SectionHome() {
         <div className={styles.homeDiv}>
           <h2>업데이트내역</h2>
           <pre>
-            1. 아이템 및 패치노트 통합<br />
-            2. 캐릭터 스킬 정보 추가 중<br />
-            3. 직업별 차트 오류 수정<br />
-            4. 과거 패치노트(수작업) - 자동화 진행중<br />
+            현재
+            https://m16tool.xyz/ 사이트 인증서 만료<br />
+            데이터 가져오는 것이 위험<br />
+            검색기능 및 마이페이지 기능 잠금<br /><br />
+            • m16에서 인증서 갱신해야 해결가능<br />
           </pre>          
         </div>  
         <div className={styles.homeDiv}>
           <h2>추가예정</h2>
           <pre>
-            1. 버전별 캐릭별 평점 및 코멘트<br />
+            1. DB서버 개편(언젠가...?)<br />
           </pre>
         </div>
       </div>
