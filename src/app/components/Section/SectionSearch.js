214f59a7dcf7b686f0a2875246477242faee99a2
home <woongdam777@gmail.com>
2025-07-07 12:44:05 +0900

    fix and add update

--- src/app/components/Section/SectionSearch.js
+++ src/app/components/Section/SectionSearch.js
@@ -149,7 +149,7 @@ export default function SectionSearch() {
 }
 
 function ForceTable({ fnString }) {
-  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 15, 20, 33, 40, 50];
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 15, 20, 33, 40, 50, 100];
   const charges = fnString.split('|');
 
   return (
